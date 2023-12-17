2a9766b431d0a7c5114999c06faf4477178b6a75
6rahul9 <rahulmohature0421@gmail.com>
2023-12-17 23:23:58 +0530

    change in code

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -26,15 +26,19 @@ const Gallery = () => {
         <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Photos of our Dishesh.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
+
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04, images.gallery05, images.gallery06, images.gallery07, images.gallery08, images.gallery09, images.gallery10, images.gallery11].map((image, index) => (
+          {[images.gallery11, images.gallery10, images.gallery09, images.gallery04, images.gallery05, images.gallery06, images.gallery07, images.gallery08, images.gallery03, images.gallery02,
+           images.gallery01].map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <img src={image} alt="gallery_image" />
               <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
+
+
         <div className="app__gallery-images_arrows">
           <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
           <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
